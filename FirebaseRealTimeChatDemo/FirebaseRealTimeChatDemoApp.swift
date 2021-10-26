c50025e342daff213633a778404a13f11e560182
Jorge Mendizabal <jorge.mendizabal0@walmart.com>
2021-10-25 20:48:52 -0500

    Initial Commit

---
+++ FirebaseRealTimeChatDemo/FirebaseRealTimeChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseRealTimeChatDemoApp.swift
+//  FirebaseRealTimeChatDemo
+//
+//  Created by Jorge Mendizabal on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseRealTimeChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
